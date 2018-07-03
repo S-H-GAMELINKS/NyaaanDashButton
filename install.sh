18d8015160e48a884fe77e07994c6880096c1987
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-04 08:47:20 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt install python3
+
+sudo apt install curl
+
+curl -O https://bootstrap.pypa.io/get-pip.py
+
+python3 get-pip.py
+
+pip install amazon-dash
+
+touch amazon-dash.yml
+
+sudo chown root:root amazon-dash.yml
